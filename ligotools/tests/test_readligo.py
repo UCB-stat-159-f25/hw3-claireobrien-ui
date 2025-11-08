ac568d5e4f1313e824a060db85e7ad147477fe01
Claire O'Brien <claire.obrien@berkeley.edu>
2025-11-08 07:27:56 +0000

    actual final updates

--- ligotools/tests/test_readligo.py
+++ ligotools/tests/test_readligo.py
@@ -1,3 +1,4 @@
 from ligotools import readligo as rl
 import numpy as np
 
+# could not get to this because I was struggling with the death of one of my extended family members, but I would have written two unit tests for this example. One of the unit tests would have checked if read_hdf5 was reading correctly, and I would have done this by supplying toy data. The other would have been a test for the loaddata function to ensure that the outputs were not changing when the data was loaded. 
--- ligotools/tests/test_utils.py
+++ ligotools/tests/test_utils.py
@@ -1,3 +1,4 @@
 from ligotools.utils import whiten, write_wavfile, reqshift
 import numpy as np
 
+# could not get to this because I was struggling emotionally with a death in my extended family, but if I could have, I would have done two unit tests. One would have looked at whether the wavfile was actually being created correctly and outputting expected data. The other would have checked the expected outputs and dimensions of reqshift. 
